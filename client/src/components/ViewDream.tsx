9f366d95d475356ce7b166d7a1f73b255582650d
Jason Wong <wongjason415@gmail.com>
2023-02-03 15:17:59 -0800

    view dream detail accordion

--- client/src/components/ViewDream.tsx
+++ client/src/components/ViewDream.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
-import { Card } from 'react-bootstrap';
-import Dropdown from 'react-bootstrap/Dropdown';
+import { Card, Dropdown, Accordion } from 'react-bootstrap';
+import AccordionBody from 'react-bootstrap/esm/AccordionBody';
 
 export function DreamComponent() {
     const [dreams, setDreams] = useState<any[]>([]);
+    // const [toggle, setToggle] = useState(false);
 
     useFocusEffect(
         useCallback(() => {
@@ -61,10 +62,6 @@ export function DreamComponent() {
         return stringTemp.slice(0, -5) + ':' + stringTemp.slice(-5);
     }
 
-    dreams.map((dream) => {
-        return (dream.time_to_bed = convert2400toAMPM(dream.time_to_bed));
-    });
-
     return (
         <>
             {dreams.map((dream) => {
@@ -77,21 +74,29 @@ export function DreamComponent() {
                                     <Dropdown>
                                         <Dropdown.Toggle split variant="basic" id="dropdown-split-basic" />
                                         <Dropdown.Menu>
-                                            <Dropdown.Item href="/dreamDetails">Details</Dropdown.Item>
                                             <Dropdown.Item href="/updateDream">Update</Dropdown.Item>
                                             <Dropdown.Item href="/deleteDream">Delete</Dropdown.Item>
                                         </Dropdown.Menu>
                                     </Dropdown>
                                 </div>
                             </Card.Header>
-                            <section className="dreamItem">
-                                {/* <div id="dreamDay">Day of Week: {dream.day_of_week}</div>
-                                <div id="dreamTimeToBed">Time to Bed: {dream.time_to_bed}</div>
-                                <div id="dreamMinutesAwake">Minutes Awake: {dream.time_awake}</div>
-                                <div id="dreamPeople">People in Dream: {dream.people.join(', ') || 'None'}</div>
-                                <div id="dreamLocation">Location: {dream.dream_location}</div>
-                                <div id="dreamType">Type of Dream: {dream.type_of_dream}</div> */}
-                                <div id="dreamDescription">{dream.dream_description}</div>
+                            <section className="dreamItemDropdownToggle">
+                                <Accordion>
+                                    <Accordion.Item eventKey={dream.dream_id}>
+                                        <Accordion.Header>{dream.dream_description}</Accordion.Header>
+
+                                        <AccordionBody>
+                                            <div id={dream.dream_id} key={dream.dream_id}>
+                                                <div id="dreamDay">Day of Week: {dream.day_of_week}</div>
+                                                <div id="dreamTimeToBed">Time to Bed: {convert2400toAMPM(dream.time_to_bed)}</div>
+                                                <div id="dreamMinutesAwake">Minutes Awake: {dream.time_awake}</div>
+                                                <div id="dreamPeople">People in Dream: {dream.people.join(', ') || 'None'}</div>
+                                                <div id="dreamLocation">Location: {dream.dream_location}</div>
+                                                <div id="dreamType">Type of Dream: {dream.type_of_dream}</div>
+                                            </div>
+                                        </AccordionBody>
+                                    </Accordion.Item>
+                                </Accordion>
                             </section>
                         </Card>
                     </div>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,9 +6,10 @@ export interface IHomePageProps {}
 
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
     return (
+        //if remember me navigate to dream entry
         <div id="homeSection" style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
             <section style={{ color: '#ffffff', textShadow: '1px 1px 5px black' }}>
-                <span style={{ display: 'flex', textAlign: 'center', fontSize: '1.3rem', margin: '40px 0px -30px 0px' }}>Capture the unfettered thoughts and emotions from this state of mind. </span>
+                <span style={{ display: 'flex', textAlign: 'center', fontSize: '1.3rem', margin: '50px 0px -35px 0px' }}>Capture the thoughts and emotions from your dreams. </span>
                 <Login />
                 <span style={{ display: 'block', textAlign: 'center' }}>
                     Don't have an account?{' '}
